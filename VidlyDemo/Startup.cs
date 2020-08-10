da59062738d47e2b1c3b6c688bcfc9db79c1fe7e
Madhan kumar <madhan5253@gmail.com>
2020-08-10 16:47:23 +0530

    Add project files.

---
+++ VidlyDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyDemo.Startup))]
+namespace VidlyDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
